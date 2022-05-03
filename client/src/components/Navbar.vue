c932c74bce7488706513df2de065cc6072be10a9
Radu Aprofiri <raduaprofiri@gmail.com>
2022-05-03 13:47:52 +0300

    added postman collection

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="/">Interviu</a>
+      <a class="navbar-brand" href="/">TGM</a>
       
       <button
         class="navbar-toggler"
